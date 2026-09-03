7a73981b404c596689ddf6f5c31b8c27cbb2b3a0
Dimas BN <120348689+dimas-bn@users.noreply.github.com>
2026-09-03 15:27:12 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ export default function App() {
           Versi Terbaru
         </span>
         <span>
-          JMO kini hadir dengan 🚀 <strong>Papan Poin Live</strong> dan Sistem yang ⚡ <strong>Cepat, Sederhana, Mudah digunakan</strong>
+          JMO kini hadir dengan 🚀 <strong>Papan Poin Live</strong> dan Sistem yang ⚡ <strong>Cepat, Sederhana, No Ribet</strong>
         </span>
         <a
           href="#demo-interaktif"
