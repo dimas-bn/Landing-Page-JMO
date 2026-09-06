6dc8a47cf695dc9c0ede854f063d48f0164c7b6b
Dimas BN <120348689+dimas-bn@users.noreply.github.com>
2026-09-06 21:25:10 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -159,7 +159,7 @@ export const Footer: React.FC = () => {
             <img
              src="/avatar-dimas.png"
              alt="Dimas BN"
-             className="w-9 h-9 rounded-full inline-block align-baseline mx-1"
+             className="w-9 h-9 rounded-full inline-block align-middle mx-1"
              /> 
               Dimas BN
             </a>
